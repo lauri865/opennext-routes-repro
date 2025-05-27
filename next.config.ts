646037f7f2a6209d800dce40269ee523b4d44305
lauri865 <lauri.lehtmaa@gmail.com>
2025-05-27 10:48:50 +0200

    new repro

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/:path",
+          destination: "https://github.com/opennextjs/opennextjs-aws/pull/858",
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 initOpenNextCloudflareForDev();
